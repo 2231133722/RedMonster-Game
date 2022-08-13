487c1e8a025cbc3f04f7803bc5f16cf6f981dc56
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-13 18:58:51 -0400

    Update Patrol.cs

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -5,58 +5,63 @@ using UnityEngine.SceneManagement;
 
 public class Patrol : MonoBehaviour
 {
-	public Animator anim;
-	public float speed;
-	public bool MoveRight;
+    public Animator anim;
+    public float speed;
+    public bool MoveRight;
+    public bool canMove = true;
 
-	// Use this for initialization
-	void Update()
-	{
-		// Use this for initialization
-		if (MoveRight)
-		{
-			transform.Translate(2 * Time.deltaTime * speed, 0, 0);
-			transform.localScale = new Vector2(-1, 1);
-		}
-		else
-		{
-			transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
-			transform.localScale = new Vector2(1, 1);
-		}
-	}
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.gameObject.CompareTag("Turn"))
-		{
+    // Use this for initialization
+    void Update()
+    {
+        if (canMove == true)
+        {
+            if (MoveRight)
+            {
+                transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+                transform.localScale = new Vector2(-1, 1);
+            }
+            else
+            {
+                transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+                transform.localScale = new Vector2(1, 1);
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Turn"))
+        {
 
-			if (MoveRight)
-			{
-				MoveRight = false;
-			}
-			else
-			{
-				MoveRight = true;
-			}
-		}
-	}
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
-		if (other.gameObject.CompareTag("Player"))
-		{
-			Vector2 direction = other.GetContact(0).normal;
-			if (direction.y == -1)
-			{
-				anim.SetTrigger("Kill");
-				Destroy(this.gameObject, 0.5f);
-				
-			}
-			else {
-				SceneManager.LoadScene(2);
-			}
-		}
-	}
-	public void killObject()
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Vector2 direction = other.GetContact(0).normal;
+            if (direction.y == -1)
+            {
+                anim.SetTrigger("Kill");
+                canMove = false;
+                Destroy(this.gameObject, 0.5f);
+
+            }
+            else
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
+    }
+    public void killObject()
     {
-		Destroy(this.gameObject);
-	}
+        Destroy(this.gameObject);
+    }
 }
