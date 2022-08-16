e63e17945eb218704e44f34672d53870e9e78b7a
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-16 18:25:38 -0400

    newstuff

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private SavePoint sp;
+
+    public float speed = 20f;
+    public Rigidbody2D rb; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(new Vector2(-1, 0)* Random.Range(15f, 25f), ForceMode2D.Impulse);
+        sp = GameObject.FindGameObjectWithTag("SP").GetComponent<SavePoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+        if(other.gameObject.CompareTag("Player"))
+        other.gameObject.transform.position = sp.lastCheckPointPos;
+    }
+}
