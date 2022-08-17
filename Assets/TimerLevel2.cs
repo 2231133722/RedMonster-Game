a8303d85ef8622b4175936be765800f7daa4e950
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-17 15:52:43 -0400

    add L2 music

---
+++ Assets/TimerLevel2.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerLevel2 : MonoBehaviour
+{
+    private SavePoint sp;
+
+    public TMP_Text clockText;
+
+    public float timeRemaining = 1;
+
+    private void Start()
+    {
+        sp = GameObject.FindGameObjectWithTag("SP").GetComponent<SavePoint>();
+    }
+    void Update()
+    {
+        if(timeRemaining < 10f)
+        {
+            clockText.color = Color.red;
+        }
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            DisplayTime(timeRemaining);
+        }
+        else
+        {
+            timeRemaining = 0;
+            sp.lastCheckPointPos = sp.startCheckPointPos;
+            SceneManager.LoadScene(4);
+        }
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        float milliSeconds = (timeToDisplay % 1) * 1000;
+        clockText.text = string.Format("Timer: " + "{0:00}:{1:00}:{2:000}", minutes, seconds, milliSeconds);
+    }
+}
