c343b76dce5eb92fb3b69666907b5c6258bfc8d9
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-18 02:54:21 -0400

    bugfix

--- Assets/Blade.cs
+++ Assets/Blade.cs
@@ -7,24 +7,30 @@ public class Blade : MonoBehaviour
     private Rigidbody2D rb;
     private SavePoint sp;
     [SerializeField] private float torqueForce;
+    [SerializeField] private AudioSource myAudioSource;
+    [SerializeField] private AudioClip bladeKill;
 
     // Start is called before the first frame update
     void Start()
     {
+        myAudioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody2D>();
         sp = GameObject.FindGameObjectWithTag("SP").GetComponent<SavePoint>();
         //rb.AddTorque(torqueForce, ForceMode2D.Force);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.Rotate(0, 0, torqueForce);
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
+        {
+            myAudioSource.PlayOneShot(bladeKill, 0.7f);
             other.gameObject.transform.position = sp.lastCheckPointPos;
+        }
     }
 
 }
