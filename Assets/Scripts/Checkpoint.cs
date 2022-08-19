b5244b4f74a5b3656d8c2c55c9bb5c572de24bd9
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-19 01:08:21 -0400

    still broken

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    MYJSON myJSON = new MYJSON();
+
+    private Timer myTimer;
+
     private SavePoint sp;
+
+    string json;
 
     private void Start()
     {
+        myJSON.timeRemaining = 0;
+        json = JsonUtility.ToJson(myJSON);
+        myTimer = GameObject.Find("Timer").GetComponent<Timer>();
         sp = GameObject.FindGameObjectWithTag("SP").GetComponent<SavePoint>();
     }
     private void OnTriggerEnter2D(Collider2D other)
@@ -15,6 +24,8 @@ public class Checkpoint : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             sp.lastCheckPointPos = transform.position;
+            myJSON.timeRemaining = myTimer.timeRemaining;
+            json = JsonUtility.ToJson(myJSON);
         }
     }
 }
---
+++ Assets/Scripts/MYJSON.cs
@@ -0,0 +1,5 @@
+
+public class MYJSON 
+{
+    public float timeRemaining;
+}
