28fc939a2a49dd9642338a7318d1a7a2328f6df6
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-19 07:09:30 -0400

    cleanup

--- Assets/Scripts/SpikesScript.cs
+++ Assets/Scripts/SpikesScript.cs
@@ -15,11 +15,6 @@ public class SpikesScript : MonoBehaviour
         sp = GameObject.FindGameObjectWithTag("SP").GetComponent<SavePoint>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
