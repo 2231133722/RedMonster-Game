31ea507acf68b77e4cfe0c00441089166a61e9a8
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-18 20:21:36 -0400

    level2 background

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
     public static Score Instance { get; private set; }
     public int playerScore;
     private int playerStartScore;
+    private Scene activeScene;
 
     public TMP_Text scoreText;
+
+    
 
     private void Awake()
     {
@@ -19,8 +23,21 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerStartScore = 0;
-        scoreText.text = "Score: " + playerStartScore;
+        activeScene = SceneManager.GetActiveScene();
+        if (activeScene == SceneManager.GetSceneByBuildIndex(2))
+        {
+            playerStartScore = 0;
+            scoreText.text = "Score: " + playerStartScore;
+        }else
+        {
+            scoreText.text = "Score: " + playerScore;
+        }
+        //else
+        //{
+        //    playerStartScore = PlayerPrefs.GetInt("playerStartScore");
+        //}
+        
+        
     }
 
     // Update is called once per frame
