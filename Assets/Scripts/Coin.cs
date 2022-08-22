342ea4f22bf21bb826536326f0886eec0c986653
quomitter <109629602+quomitter@users.noreply.github.com>
2022-08-22 07:51:14 -0400

    Update Coin.cs

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -5,20 +5,14 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public AudioClip aCoin1;
-    AudioSource audioSource2;
-
-    private void Start()
-    {
-        audioSource2 = GetComponent<AudioSource>();
-    }
-
+  
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player")
         {
             Score.Instance.AddHundoPeice();
-            audioSource2.PlayOneShot(aCoin1, 0.7F);
-            Destroy(gameObject, 0.1f);
+            AudioSource.PlayClipAtPoint(aCoin1, Camera.main.transform.position, 0.7F);
+            Destroy(gameObject);
         }
     }
 }
